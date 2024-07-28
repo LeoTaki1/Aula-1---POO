80c20242beaf8f8f5f84af61a97ed03d3776b87e
LeoTaki1 <leonardo.taki@gmail.com>
2024-07-28 15:13:12 +0000

    Salvar

---
+++ src/PassagemArgumentos.java
@@ -0,0 +1,41 @@
+public class PassagemArgumentos {
+    /*
+     * Duas tecnicas para passar argumentos:
+     * - passagem por valor (somente os tipos primitivos)
+     * - passagem por referência (somente os tipos de referência (objetos))
+     */
+
+     public static int f(int a, int b) {
+        a++;
+        b++;
+
+        return a + b;
+     }
+
+     public static void incrementa(int[] v) {
+        for (int i = 0; i < v.length; i++) {
+            v[i]++;
+        }
+     }
+
+     public static void testaPorValor() {
+        int a = 10;
+        int b = 20;
+
+        System.out.printf("a = %d, b = %d\n", a, b);
+        f(a, b);
+        System.out.printf("a = %d, b = %d\n", a, b);
+     }
+
+     public static void testaPorReferencia() {
+        
+     }
+     public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.printf("a = %d, b = %d\n", a, b);
+        f(a, b);
+        System.out.printf("a = %d, b = %d\n", a, b);
+     }
+}
